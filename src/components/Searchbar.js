5ad11199a1548741a74b4b77d519496141935594
Josh Brown <joshuaebrown021@gmail.com>
2022-04-29 19:28:27 -0400

    added with_router

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,16 +1,13 @@
-// Seachbar.js
-import { useContext } from 'react'
-import { SearchContext} from '../context/SearchContext'
+import { useState } from 'react'
 
-function Searchbar() {
-    const [term, handleSearch] = useContext(SearchContext)
+const Searchbar = (props) => {
+    let [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <form>
-
-            <input ref={term} type="text" placeholder="Search Here" />
-            <button onClick={(e)=> handleSearch(e, term.current.value)}>Submit</button>
-        </form>
+            <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+                <input type="text" placeholder="Search Here" onChange={(e) => setSearchTerm(e.target.value)} />
+                <input type="submit" />
+            </form>
     )
 }
 
